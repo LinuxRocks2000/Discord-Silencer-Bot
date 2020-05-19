d6476586b0780c34b6077d22dd960860b8fb71f2
LinuxRocks2000 <you@example.com>
2020-05-19 17:59:13 -0400

    f

--- silencerBot.py
+++ silencerBot.py
@@ -22,8 +22,14 @@ def isTrue(dict):
         if dict[i]==True:
             trues.append(i)
     return trues
-indicators={'a': '\U0001f1e6', 'b': '\U0001f1e7', 'c': '\U0001f1e8', 'd': '\U0001f1e9', 'e': '\U0001f1ea', 'f': '\U0001f1eb', 'g': '\U0001f1ec', 'h': '\U0001f1ed', 'i': '\U0001f1ee', 'j': '\U0001f1ef', 'k': '\U0001f1f0', 'l': '\U0001f1f1', 'm': '\U0001f1f2', 'n': '\U0001f1f3', 'o': '\U0001f1f4', 'p': '\U0001f1f5', 'q': '\U0001f1f6', 'r': '\U0001f1f7', 's': '\U0001f1f8', 't': '\U0001f1f9', 'u': '\U0001f1fa', 'v': '\U0001f1fb', 'w': '\U0001f1fc', 'x': '\U0001f1fd', 'y': '\U0001f1fe', 'z': '\U0001f1ff'}
-days='mtwhf'
+def make(letter): ## Python magic does the same as the original thing
+	lets="abcdefghijklmnopqrstuvwxyz"
+	maked=eval('''"\\U000'''+hex(lets.index(letter)+127462)[2:]+'''"''')
+	return maked
+async def addReactWord(word,message):
+    for x in word:
+        await message.add_reaction(make(x))
+#days='mtwhf'  ## As this robot seems more inclined to reek havoc than anything else, no need for a day counter.
 
 client = discord.Client()
 
@@ -33,14 +39,16 @@ async def on_ready():
     await client.change_presence(activity=discord.Game(name="be quiet."))
 @client.event
 async def on_message(message):
+    if message.author==client.user:
+        return ## Terminates before anything else. Not perhaps the most sensical way to do it, but it works.
     if message.content.startswith('tsb!'):
-        if message.content.startswith('tsb!perms'):
+        args=message.content.split(" ") ## Use split to get argument list.
+        if args[1]=="perms":
             await message.channel.send('I have the permissions: '+str(isTrue(makeDict(iter((message.channel.permissions_for(message.channel.guild.me)))))))
         else:
             await message.channel.send("That's not a thing.")
     elif not message.author.bot and not hasRole('The Silent Few',message.author.roles) and message.author!=client.user and not (message.content.startswith('*') and message.content[1]!='*' and not message.content[-2] in ['\\','*'] and message.content[-1]=='*'):
         await message.channel.send(message.author.name+' was silenced.')
-        for i in 'quiet':
-            await message.add_reaction(indicators[i])
+        await addReactWord("quiet",message)
         await message.author.ban(reason='Being too loud.')
 client.run(input('What is the bot\'s token?'))
